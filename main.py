9346e6c64d061ab6eab2d7d58704d8419ff1c39b
Kidist Meseret <kidistmeseret1@gmail.com>
2026-05-08 15:21:42 +0900

    update

--- main.py
+++ main.py
@@ -151,9 +151,7 @@ def generate_maze_step():
         elif direction == 'E':
             eastWall[i][j] = 0
 
-        current = (ni, nj)
-
-        visited[ni][nj] = True
+        
 
     elif stack:
         current = stack.pop()
